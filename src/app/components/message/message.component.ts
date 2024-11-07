db8c5ab651564dd17a17a1ca19495e940188d7df
CrnTarim <crntrim@gmail.com>
2024-11-07 23:14:14 +0300

    messageservice,component,signalr

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,65 +1,35 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Message } from '../../models/message';
 import { MessagesService } from '../../services/messages.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrl: './message.component.css'
 })
-export class MessageComponent implements OnInit {
-  messages: Message[] = [];
-  newMessage: string = '';
-  senderId: string = '';
-  receiverId: string = '263bf8e5-1a5f-42ec-8a2b-1e0ee171141b'; 
+export class MessageComponent  {
+  message: string = '';  
+  targetConnectionId: string = '';  
+  receivedMessages: string[] = []; 
 
-  constructor(private messagesService: MessagesService,private cdr: ChangeDetectorRef) {}
+  constructor(private messagesService: MessagesService) { }
 
   ngOnInit(): void {
-    
-    this.senderId = localStorage.getItem('userId') || '';
-
    
-    this.messagesService.getMessages(this.senderId).subscribe(contents => {
-      contents.forEach(content => {
-        this.messages.push({
-          senderId: this.senderId,
-          receiverId: this.receiverId,
-          content
-        });
-      });
+    this.messagesService.message$.subscribe(message => {
+      if (message) {
+        this.receivedMessages.push(message);  
+      }
     });
-
-  
-    this.messagesService.messageReceived$.subscribe(message => {
-      console.log('Yeni gelen mesaj:', message);
-      const newMessage: Message = {
-          senderId: message.senderId,
-          receiverId: message.receiverId,
-          content: message.content
-      };
-      this.messages.push(newMessage);
-      this.cdr.detectChanges(); // Değişiklikleri algıla
-  });
-    
   }
 
+  // Hedef kullanıcıya mesaj gönder
   sendMessage(): void {
-    if (this.newMessage.trim()) {
-      const message: Message = {
-        senderId: this.senderId,
-        receiverId: this.receiverId,
-        content: this.newMessage
-      };
-  
-      this.messagesService.postMessage(message).subscribe(response => {
-        console.log('API üzerinden mesaj gönderildi:', response.message);
-        this.messagesService.sendMessageThroughSignalR(message);
-      }, error => {
-        console.error('Mesaj gönderiminde hata:', error);
-      });
-  
-      this.newMessage = '';
+    if (this.targetConnectionId && this.message.trim()) {
+      this.messagesService.sendMessageToUser(this.targetConnectionId, this.message);
+      this.message = ''; 
     }
   }
 }
--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,63 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Message } from '../models/message';
-import { Observable,Subject} from 'rxjs';
+import { BehaviorSubject, catchError, Observable,Subject, throwError} from 'rxjs';
 import * as signalR from '@microsoft/signalr';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
-  private url = 'https://localhost:7151/api/Message'; 
-  private hubConnection!: signalR.HubConnection;
-  private messageReceived = new Subject<Message>(); 
+  private hubConnection: HubConnection;
 
-  messageReceived$ = this.messageReceived.asObservable(); 
+  private messageSource = new BehaviorSubject<string>('');  
+  message$ = this.messageSource.asObservable();
 
-  constructor(private http: HttpClient) {
-    this.startSignalRConnection(); 
-  }
-
- 
-  private startSignalRConnection() {
-    this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl('https://localhost:7151/chatHub') 
+  private currentConnectionId: any;  
+  constructor() {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl('https://localhost:7151/chatHub')  
       .build();
 
-    this.hubConnection
-      .start()
+    this.hubConnection.start()
       .then(() => {
-        console.log('SignalR bağlantısı başarılı');
+        console.log('SignalR bağlantısı kuruldu');
+        this.currentConnectionId = this.hubConnection.connectionId; 
+        console.log('Connection ID:', this.currentConnectionId);  
       })
-      .catch(err => console.error('SignalR bağlantı hatası: ', err));
-
-    
-      this.hubConnection.on('ReceiveMessage', (senderId: string, receiverId: string, content: string) => {
-        console.log('Mesaj alındı:', { senderId, receiverId, content }); 
-        const receivedMessage: Message = {
-          senderId,
-          receiverId,
-          content
-        };
-        this.messageReceived.next(receivedMessage); 
-      });
-           
-  }
-
-  postMessage(message: Message): Observable<any> {
-    return this.http.post<any>(`${this.url}/sendmessage`, message);
-  }
-
-  getMessages(userId: string): Observable<string[]> {
-    return this.http.get<string[]>(`${this.url}/${userId}`);
-  }
-
- 
-  sendMessageThroughSignalR(message: Message) {
-    this.hubConnection.invoke('SendMessage', message.senderId, message.receiverId, message.content)
       .catch(err => {
-        console.error('SignalR üzerinden mesaj gönderme hatası: ', err);
+        console.error('Bağlantı hatası:', err);
       });
+
+    this.hubConnection.on('ReceiveMessage', (message: string) => {
+      this.messageSource.next(message);  
+    });
   }
-  
+
+  private isConnectionReady(): boolean {
+    return this.hubConnection.state === HubConnectionState.Connected;
+  }
+ 
+  sendMessageToUser(targetConnectionId: string, message: string): void {
+    if (this.isConnectionReady() && targetConnectionId && message.trim()) {
+      
+      this.hubConnection.invoke('SendMessageToUser', targetConnectionId, message)
+        .catch(err => console.error('Mesaj gönderme hatası:', err));
+    } else {
+      console.error('Bağlantı kurulamadı veya geçerli bağlantı ID bulunamadı');
+    }
+  } 
 }
